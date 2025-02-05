a392cfd922d9f388c376b6d90d6ea11998851ab1
Jovani Mansandes <72328711+jovani11@users.noreply.github.com>
2025-02-05 19:07:46 +0800

    Update send.js

--- send.js
+++ send.js
@@ -2,5 +2,5 @@ function sendMail(value) {
     var parms = {
         message : value
     }
-    emailjs.send("service_gljppwl", "template_x6l6v7t",parms).then(alert("Response has been submitted"))
+    emailjs.send("service_gljppwl", "template_x6l6v7t",parms)
 }
